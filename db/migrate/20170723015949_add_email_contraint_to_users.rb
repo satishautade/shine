7649293a47599690358c7489d2d9e3665c937bdf
Satish Autade <satishautade@gmail.com>
2017-07-23 13:44:03 +1000

    Added constraint for email to be something@example.something in DB and created migration scripts necessary

---
+++ db/migrate/20170723015949_add_email_contraint_to_users.rb
@@ -0,0 +1,20 @@
+class AddEmailContraintToUsers < ActiveRecord::Migration[5.1]
+  def up
+    execute %{
+      ALTER TABLE
+        users
+      ADD CONSTRAINT
+        email_must_be_company_email
+        CHECK ( email ~* '^[^@\s]+@example\\.[^@\s]+$')
+    }
+  end
+
+  def down
+    execute %{
+      ALTER TABLE
+        users
+      DROP CONSTRAINT
+        email_must_be_company_email
+    }
+  end
+end
