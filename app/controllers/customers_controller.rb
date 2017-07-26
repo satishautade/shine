017d1cdba1725d9531e23f88857c38062f65b243
Satish Autade <satishautade@gmail.com>
2017-07-26 21:56:23 +1000

    Display each result in the component layout and implement pagination

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,12 +1,15 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
   def index
+    @page = (params[:page] || 0).to_i
     if params[:keywords].present?
       @keywords = params[:keywords]
       customers_search_term = CustomerSearchTerm.new(@keywords)
       @customers = Customer.where(
           customers_search_term.where_clause,
           customers_search_term.where_args).
-          order(customers_search_term.order)
+          order(customers_search_term.order).
+          offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @customers = []
     end
