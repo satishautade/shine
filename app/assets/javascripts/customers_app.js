210c2c6a6cc22d23207ad788a0253fd5be94e26a
Satish Autade <satishautade@gmail.com>
2017-07-27 02:22:53 +1000

    Angularised Search: Needs clicking Search button

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -7,10 +7,25 @@ var app = angular.module('customers', []);
 // };
 
 app.controller("CustomerSearchController",
-    ["$scope",
-        function($scope) {
-            $scope.search = function(searchTerm) {
+    ["$scope", "$http",
+        function ($scope, $http) {
+            $scope.customers = [];
+            // search function defined in the passed Angular Scope, requires a searchTerm param
+            $scope.search = function (searchTerm) {
+                // Display the Search term on the page
                 $scope.searchedFor = searchTerm;
+                // Fire an AJAX request to Server (CustomersController class), asking JSON response
+                $http.get("/customers.json", {"params": {"keywords": searchTerm}}).
+                then(
+                    // Success Function
+                    function (response) {
+                        $scope.customers = response.data;
+                    },
+                    // Failure Function
+                    function (response) {
+                        alert("There was a problem" + response.status)
+                    }
+                );
             }
         }
     ]);
