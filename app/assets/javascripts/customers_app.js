02633a8f6497f2b2b827b192d3ce972146cd37d5
Satish Autade <satishautade@gmail.com>
2017-07-27 01:33:21 +1000

    Angularise Search without any results- display only search term

---
+++ app/assets/javascripts/customers_app.js
@@ -0,0 +1,16 @@
+var app = angular.module('customers', []);
+
+// var CustomerSearchController = function($scope){
+//     $scope.search = function(searchTerm){
+//         $scope.searchedFor = searchTerm;
+//     }
+// };
+
+app.controller("CustomerSearchController",
+    ["$scope",
+        function($scope) {
+            $scope.search = function(searchTerm) {
+                $scope.searchedFor = searchTerm;
+            }
+        }
+    ]);
