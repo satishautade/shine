a1806727e81a3dda0d277e7ea2f6516d075e22b1
Satish Autade <satishautade@gmail.com>
2017-08-05 04:15:44 +1000

    Added customer search test that returns dummy results

--- spec/javascripts/controllers/customer_search_controller_spec.js
+++ spec/javascripts/controllers/customer_search_controller_spec.js
@@ -1,20 +1,56 @@
 describe("Test Angular Controller", function () {
     describe("Initialization CustomerSearchController", function () {
         // Declare variables so that they are accessible globally within test
-        var scope = null, controller = null;
-        // Load Angular App (DOES NOT START IT)
+        var scope = null,
+            controller = null,
+            httpBackend = null,
+            cannedCustomersResponse = [
+                {
+                    id: 123,
+                    first_name: "Bob",
+                    last_name: "Jones",
+                    email: "bjones@foo.net",
+                    username: "jonesy"
+                },
+                {
+                    id: 456,
+                    first_name: "Bob",
+                    last_name: "Johnson",
+                    email: "johnboy@bar.info",
+                    username: "bobbyj"
+                }
+            ];
+
+        // Mind the sequence of below code
+        // Step 1: Load Angular App (DOES NOT START IT)
         beforeEach(module('customers'))
-        // Use inject to Mock the call to Angular controller with rootScope (angular like scope)
-        beforeEach(inject(function ($controller, $rootScope) {
+
+        // Step 2: Use inject to Mock the call to Angular controller with rootScope (angular like scope)
+        beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
             scope = $rootScope.$new();
+            httpBackend = $httpBackend;
             // Call the controller using inject method and pass scope
             controller = $controller("CustomerSearchController", {
                 $scope: scope
             });
         }));
 
+        // Step 3: Configure HTTP backend to mock the request and response to server
+        beforeEach(function () {
+           httpBackend.when('GET', '/customers.json?keywords=bob&page=0').
+           respond(cannedCustomersResponse);
+        });
+
+        //TEST CASES
+
         it("Defaults to an empty customer list", function () {
             expect(scope.customers).toEqualData([]);
+        });
+
+        it("populates the customer with list of results", function () {
+            scope.search("bob");
+            httpBackend.flush();
+            expect(scope.customers).toEqualData(cannedCustomersResponse);
         });
     });
 
